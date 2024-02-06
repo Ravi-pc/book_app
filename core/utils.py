4b11c6d56ff36525bab58de44f581b41e6eeb57a
“Ravi-pc” <ravisingh98138@gmail.com>
2024-02-06 11:27:05 +0530

    [Ravi] Add: Program to authorize the user for performing operations on book API.

--- core/utils.py
+++ core/utils.py
@@ -3,29 +3,31 @@ import smtplib
 import ssl
 from datetime import datetime, timedelta
 from email.message import EmailMessage
-
 import bcrypt
 import jwt
+from sanic import response, Request
+
+from core.settings import jwt_secrete_key, sender, password, super_key
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 ALGORITHM = "HS256"
-JWT_SECRET_KEY = "b1a61d3450299222c96086499b01f00e7963216a152294f3ba8ef81db52146fc"
-Sender = "ravisingh98138@gmail.com"
-Password = "mlbs qrem subb pxdv"
-super_key = "7651929879"
+JWT_SECRET_KEY = jwt_secrete_key
+Sender = sender
+Password = password
+super_key = super_key
 
 logging.basicConfig(filename='./fundoo_notes.log', encoding='utf-8', level=logging.DEBUG,
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%y %I:%M:%S %p')
 logger = logging.getLogger()
 
 
-def hash_password(password: str) -> str:
+def hash_password(plain_password: str) -> str:
     """
         Description: hash_password function is used to hash the password.
         Parameter: normal password.
         Return: hashed password.
     """
-    hashed_password_bytes = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    hashed_password_bytes = bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
     hashed_password_str = hashed_password_bytes.decode('utf-8')
     return hashed_password_str
 
@@ -54,6 +56,23 @@ def decode_token(token):
     return jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
 
 
+async def authorize(request: Request):
+    token = request.token
+
+    if not token:
+        return response.json({"message": "Unauthorized: Missing Authorization Header"}, status=401)
+
+    try:
+        decoded_token = decode_token(token)
+        user_id = decoded_token.get('user_id')
+
+        # Add user information to request state for later use
+        request.ctx.user_id = user_id
+
+    except Exception as e:
+        return response.json({"message": f"Unauthorized: {str(e)}"}, status=401)
+
+
 def email_verification(token: str, user_email):
     """
         Description: email_verification function is used to send the verification link
@@ -65,12 +84,12 @@ def email_verification(token: str, user_email):
 
     """
 
-    sender = Sender
-    password = Password
+    sender_email = Sender
+    sender_password = Password
     subject = 'Email Verification'
     body = f"Click the link to verify your email: http://127.0.0.1:8000/verify?token={token}"
     e_mail = EmailMessage()
-    e_mail['From'] = sender
+    e_mail['From'] = sender_email
     e_mail['To'] = user_email
     e_mail['Subject'] = subject
     e_mail.set_content(body)
@@ -78,8 +97,8 @@ def email_verification(token: str, user_email):
 
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(sender, password)
-            smtp.sendmail(sender, user_email, e_mail.as_string())
+            smtp.login(sender_email, sender_password)
+            smtp.sendmail(sender_email, user_email, e_mail.as_string())
             print("Successfully")
             smtp.quit()
 
